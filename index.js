b27bdf1ad9cd128064471f54ece80dde2277bece
Emmanuel-Howard <emmanuelhoward3000@gmail.com>
2025-06-10 07:21:35 -0400

    Connects constants in index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+/* Connecting required elements */
+const map = document.querySelector("svg");
+const countries = document.querySelectorAll("path");
+const sidePanel = document.querySelector(".side.panel");
+const container = document.querySelector(".side.panel .container");
+const closeBtn = document.querySelector(".close.btn");
+const loading = document.querySelector(".loading");
+const zoomInBtn = document.querySelector(".zoom.in");
+const zoomOutBtn = document.querySelector(".zoom.out");
+const zoomValueOutput = document.querySelector(".zoom.value");
+
+/* Data Outputs */
